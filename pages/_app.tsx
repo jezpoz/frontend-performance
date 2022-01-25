62bd0bc465d1b1e9a4f5396b0daaa5641a76a158
jezpoz <jesfor94@gmail.com>
2022-01-25 10:42:49 +0100

    fix font since it didn't show correctly in chrome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,6 @@ export default function App ({ Component, pageProps }) {
 		<>
 			<Head>
 				<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-				<link
-						href="https://fonts.googleapis.com/css2?family=Roboto&display=optional"
-						rel="stylesheet"
-					/>
 			</Head>
 			<main>
 				<Component {...pageProps} />
