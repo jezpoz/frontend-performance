ea753deb8fef8efdf0965054278faac277483c03
jezpoz <jesfor94@gmail.com>
2022-01-26 03:19:25 +0100

    last touches(?)

--- components/body/index.tsx
+++ components/body/index.tsx
@@ -4,9 +4,9 @@ import styles from './body.module.scss';
 
 export default function Body({content}): JSX.Element {
   return (
-    <article className={styles.article}>
+    <article className={styles.contentWrapper}>
       {content.paragraphs.map((paragraph, index) => (
-        <section key={index} className={styles.contentWrapper}>
+        <section key={index}>
           {paragraph.title && <h2 className='article-header'>{paragraph.title.text}</h2>}
           {paragraph.body?.json && <Paragraph json={paragraph.body.json} />}
           {paragraph.images && paragraph.images.map((image, index) => <Image key={index} image={image}/>)}
