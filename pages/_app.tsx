97546eb16c417663a1fe5b7504b1066cc35dbc80
jezpoz <jesfor94@gmail.com>
2022-01-26 15:17:32 +0100

    revert hls fix . will work on this later

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -19,14 +19,6 @@ export default function App ({ Component, pageProps }) {
 				<meta name="twitter:image" content="https://frontend-performance-phi.vercel.app/android-chrome-512x512.png"></meta>
 				<meta name="twitter:site" content="@jezpoz"></meta>
 				<meta name="twitter:creator" content="@jezpoz"></meta>
-				{/* Fix video for mobile */}
-				<script
-					src="https://cdnjs.cloudflare.com/ajax/libs/hls.js/0.5.14/hls.min.js"
-					integrity="sha512-js37JxjD6gtmJ3N2Qzl9vQm4wcmTilFffk0nTSKzgr3p6aitg73LR205203wTzCCC/NZYO2TAxSa0Lr2VMLQvQ=="
-					crossOrigin="anonymous"
-					referrerPolicy="no-referrer"
-					defer
-				></script>
 			</Head>
 			<main>
 				<Component {...pageProps} />
