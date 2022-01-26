d6819c845b3bcbc24eda2b3de633be213ab276e5
jezpoz <jesfor94@gmail.com>
2022-01-26 03:31:32 +0100

    fix text spacing

--- components/body/index.tsx
+++ components/body/index.tsx
@@ -6,7 +6,7 @@ export default function Body({content}): JSX.Element {
   return (
     <article className={styles.contentWrapper}>
       {content.paragraphs.map((paragraph, index) => (
-        <section key={index}>
+        <section key={index} className={styles.sectionWrapper}>
           {paragraph.title && <h2 className='article-header'>{paragraph.title.text}</h2>}
           {paragraph.body?.json && <Paragraph json={paragraph.body.json} />}
           {paragraph.images && paragraph.images.map((image, index) => <Image key={index} image={image}/>)}
